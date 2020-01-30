4330e7315260921ae9449c7fe194fda4bb5af75a
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2020-01-30 12:53:24 +0530

    updated

--- tSquares.m
+++ tSquares.m
@@ -2,7 +2,7 @@ classdef tSquares < matlab.unittest.TestCase
     
     methods (Test)
         function test1(testCase)
-            testCase.verifyEqual(squares(10), 100)
+            testCase.verifyEqual(squares(10), 101)
         end
         
         function test2(testCase)
